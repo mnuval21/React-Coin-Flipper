1425c134bab02d90b0eadb694b3916a24bb917da
Melissa Nuval <mnuval21@gmail.com>
2022-04-09 21:43:21 -0400

    first commit. a basic coin flipper with react

---
+++ src/CoinContainer.js
@@ -0,0 +1,54 @@
+import React, { Component} from 'react';
+import Coin from  './Coin';
+import { choice } from './helpers';
+
+class CoinContainer extends Component {
+    static defaultProps = {
+        coins: [
+            {side: 'heads', imgSrc: "https://bjc.edc.org/June2017/bjc-r/img/5-algorithms/img_flipping-a-coin/Heads.png"},
+            {side: 'tails', imgSrc: "https://bjc.edc.org/June2017/bjc-r/img/5-algorithms/img_flipping-a-coin/Tails.png"}
+        ]
+    };
+  
+    constructor(props){
+        super(props);
+        this.state = {
+            currCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0,
+        };
+        this.handleClick = this.handleClick.bind(this);  
+    }
+
+    flipCoin() {
+        const newCoin = choice(this.props.coins);  
+        this.setState(st => {
+            return {
+                currCoin: newCoin,
+                nFlips: st.nFlips + 1,
+                nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+                nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
+            };
+        });      
+    }
+  
+    handleClick(e) {
+        this.flipCoin();
+    }
+
+    render() {
+        return (
+            <div className="CoinContainer">
+                <h2>Let's Flip A Coin</h2>
+                <p>Heads I win, Tails you lose</p>
+                {this.state.currCoin && <Coin info={this.state.currCoin} />}
+                <button onClick={this.handleClick}> Flip Me! </button>
+
+                <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>     
+            </div>
+        )
+    }
+}
+
+export default CoinContainer;
